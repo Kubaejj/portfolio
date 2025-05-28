08926066b9f36bf93467de9fc2fe5f3695b2d5dd
kubaejj <jakub.jezbera.023@pslib.cz>
2025-05-28 20:21:54 +0200

    portfiloio 1st done

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let hamElements = document.querySelectorAll(".header__hamburger, .header__nav");
-    for (const btn of document.querySelectorAll(".header__hamburger")) {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
-            for (const element of hamElements) {
-                element.classList.toggle("active");
-            }  
+    const hamburger = document.querySelector(".header__hamburger");
+    const nav = document.querySelector(".header__nav");
+    const navLinks = document.querySelectorAll(".nav__item");
+
+    hamburger.addEventListener("click", (e) => {
+        e.preventDefault();
+        hamburger.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", () => {
+            hamburger.classList.remove("active");
+            nav.classList.remove("active");
         });
-    }
+    });
 });
